07561f684433a5955b951d338fe7fb673618b1bc
rjam16 <jam23019@byui.edu>
2025-04-12 00:56:46 -0700

    add required

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,10 +2,11 @@
 const regValidate = require('../utilities/account-validation')
 const express = require("express")
 const router = new express.Router() 
+const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
 
 // Route to build login view
-router.get("/login", utilities.handleErrors(accountController.buildLogin))
+router.get('/login', utilities.handleErrors(accountController.buildLogin))
 //Route for login
 router.get('/login', accountController.buildLogin);
 router.post('/register', accountController.handleRegister);
